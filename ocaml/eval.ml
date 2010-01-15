73aacbd4e6ef2cf9a7a66b50a0f750c4764dc7fa
Thanassis Avgerinos <thanassis@cmu.edu>
2010-01-15 19:38:53 +0000

    making the evaluator conform to the BIL operational semantics
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/traces@3908 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/eval.ml
+++ ocaml/eval.ml
@@ -1,4 +1,24 @@
-(* A module to perform AST-evaluation *)
+(** A module to perform AST-evaluation *)
+
+(** Things that you should be aware before using it:
+ *
+ *  - All programs should end with a halt 'value'
+ *    (not having a halt will result in an error message 
+ *
+ *  - The LOAD-table and STORE-table abilities are not
+ *    implemented yet. `Specials' are also not handled yet.
+ *
+ *  - The behaviour of the evaluator should be faithful to
+ *    the operational semantics of BIL (as provided by the 
+ *    BAP Handbook.
+ *)
+
+(** TODO list:
+ *   - Test it on several IL programs
+ *   - Add feature for zero default values of unbound vars
+ *   - Add feature to provide values of unbound vars
+ *   - Cleanup Load-Store to make more readable
+ *)
 
   open Ast
   open Type
@@ -7,8 +27,14 @@
   type addr = int64
   type instr = stmt
   type varid = Ast.exp
-  type varval = Ast.exp (* Int of int | Mem of mem | Str of string | Unit *)
+  type varval = Val of Ast.exp | Mem of (Ast.exp, Ast.exp) Hashtbl.t
   type label_kind = label
+
+  exception ExcState of string * addr
+
+(* Context update *)
+  let context_update = Hashtbl.replace
+  let context_copy = Hashtbl.copy
 
 (* Lookup functions for basic contexts *)
   let inst_fetch sigma pc =
@@ -21,190 +47,219 @@
 
   let lookup_var delta var =
     try Hashtbl.find delta var
-    with Not_found -> failwith ("uninitialized variable "^(Var.name var))
+    with Not_found -> 
+      match Var.typ var with
+        | TMem _ -> 
+          let newmemory = Mem (Hashtbl.create 1000) in
+          context_update delta var newmemory ;
+          newmemory
+        | _ -> failwith ("uninitialized variable "^(Var.name var))
 
   let lookup_mem mu index =
     try Hashtbl.find mu index 
     with Not_found -> failwith "uninitialized memory"
 
-(* Context update *)
-  let context_update = Hashtbl.replace
-  let context_copy = Hashtbl.copy
+(* Wrapping - Unwrapping functions *)
+
+  let wrap_val v = Val v
+  let unwrap_val = function 
+    | Val v -> v
+    | Mem _ -> failwith "unwrapping a memory, not a value"
+
+  let wrap_mem m = Mem m
+  let unwrap_mem = function
+    | Mem m -> m
+    | Val _ -> failwith "unwrapping a value, not a memory"
 
 (* Context Contents *)
 
   let print_values delta =
-    Printf.printf "values:\n" ;
+    Printf.printf "contents of variables:\n" ;
     Hashtbl.iter 
       (fun k v ->
         match k,v with 
-          | var,Int (n,_) -> Printf.printf "%s = %s\n" (Var.name var) (Int64.to_string n)
+          | var,Val (Int (n,_)) -> Printf.printf "%s = %s\n" (Var.name var) (Int64.to_string n)
           | _ -> ()
       ) delta
 
   let print_mem memory =
-    Printf.printf "memory:\n" ;
+    Printf.printf "contents of memories:\n" ;
     Hashtbl.iter 
       (fun k v ->
         match k,v with 
-          | Int (v,_),Int (n,_) -> Printf.printf "%s -> %s\n" (Int64.to_string v) (Int64.to_string n)
+          | var,Mem mem -> 
+             Printf.printf "memory %s:\n" (Var.name var) ; 
+             Hashtbl.iter
+               (fun k1 v1 -> 
+                 match k1,v1 with
+                  | Int (n1,_), Int (n2,_) ->
+                    Printf.printf "%s -> %s\n" (Int64.to_string n1) (Int64.to_string n2)
+                  | _ -> ()
+               ) mem
           | _ -> ()
       ) memory
 
 
   (* Evaluate an expression in a context Delta,Mu *)
-  let rec eval_expr (delta,mu,expr) =
+  let rec eval_expr (delta,expr) : varval =
     let get_expr e = 
-          match eval_expr (delta,mu,e) with
-           | Int (v,t) -> (v,t)
-           | _ -> failwith "expression cannot be evaluated"
+          match eval_expr (delta,e) with
+           | Val (Int (v,t)) -> (v,t)
+           | Val v -> failwith ("expression cannot be evaluated:\n"^(Pp.ast_exp_to_string v))
+           | Mem _ -> failwith "tried to perform memory operations"
     in
     let eval = function 
       | Var v -> 
         lookup_var delta v
       | Int _ as value -> 
-        value
+        Val value
       | Lab _ as labl ->
-        labl
+        Val labl
       | BinOp (op,e1,e2) ->
         let v1 = get_expr e1 
         and v2 = get_expr e2 in
         let (v,t) = Arithmetic.binop op v1 v2 in
-        Int (v,t)
+        Val (Int (v,t))
       | UnOp (op,e) ->
         let v = get_expr e in
         let (v,t) = Arithmetic.unop op v in
-        Int (v,t)
+        Val (Int (v,t))
+      | Cast (ct,t,e)  -> 
+        let (n,t2) = get_expr e in
+        let (n',t') = Arithmetic.cast ct (n,t2) t in
+         Val (Int (n',t'))
       | Let (var,e1,e2) ->
-        let v1 = eval_expr (delta,mu,e1) in
+        let v1 = eval_expr (delta,e1) in
         let delta' = context_copy delta in
         context_update delta' var v1 ;
-        let v2 = eval_expr (delta',mu,e2) in
+        let v2 = eval_expr (delta',e2) in
         v2
       | Load (e1,e2,e3,t) ->
-        let v1 = eval_expr (delta,mu,e1) 
-        and v2 = eval_expr (delta,mu,e2)
-        and v3 = eval_expr (delta,mu,e3) in
-        let arr = BinOp (PLUS,v1,v2) in
+        let v1 = eval_expr (delta,e1) 
+        and v2 = eval_expr (delta,e2)
+        and v3 = eval_expr (delta,e3) in
         (match t with
           | Array _ -> (* LOAD_array *)
             failwith "loading array currently unsupported"
           | Reg bits -> (* Load to register *)
             let n = bits/8 in (* FIXME: 1-bit loads? *)
             (* loading the bytes *)
+            let memory = unwrap_mem v1
+            and start_index = unwrap_val v2 in
             let rec get_bytes offset acc =
               if offset = n then acc
               else 
-                let mem_index = BinOp (PLUS,arr,Int(Int64.of_int offset, Reg 64)) in
-                let index = eval_expr (delta,mu,mem_index) in
-                let byte = lookup_mem mu index in
+                let mem_index = BinOp (PLUS,start_index,Int(Int64.of_int offset, Reg 64)) in
+                let index = unwrap_val (eval_expr (delta,mem_index)) in
+                let byte = lookup_mem memory index in
                 get_bytes (offset+1) (byte::acc)
             in
             (* changing the order according to the endianness *)
             let loaded = 
               let bytes = get_bytes 0 [] in
-              if v3 = exp_false then bytes else List.rev bytes
+              if v3 = Val exp_false then bytes else List.rev bytes
             and byte_size = Int(8L,Reg 64) in
             (* calculating the loaded value *)
             let value = 
               List.fold_left
                 (fun v n ->
                   let shl = (BinOp(LSHIFT,v,byte_size)) in
-                  BinOp(OR,shl,n)
-                ) (Int(0L,Reg 64)) loaded 
+                  unwrap_val (eval_expr (delta,BinOp(OR,shl,n)))
+                ) (Int(0L,Reg bits)) loaded 
             in
-            value
+            Val value
           | _ -> failwith "not a loadable type"
         )
       | Store (e1,e2,e3,e4,t) ->
-        let v1 = eval_expr (delta,mu,e1) 
-        and v2 = eval_expr (delta,mu,e2) 
-        and v3 = eval_expr (delta,mu,e3) in
-        let arr = BinOp (PLUS,v1,v2) in
+        let v1 = eval_expr (delta,e1) 
+        and v2 = eval_expr (delta,e2) 
+        and v3 = eval_expr (delta,e3) in
         (match t with
           | Array _ -> (* STORE_array *)
             failwith "storing array currently unsupported"
           | Reg bits -> 
-            let v4 = eval_expr (delta,mu,e4) in
-            let n = bits/8 in (* FIXME: 1-bit stores? *)
-            let lsb = 0xffffL in
+            let memory = unwrap_mem v1
+            and start_index = unwrap_val v2 
+            and v4 = eval_expr (delta,e4) 
+            and n = bits/8  (* FIXME: 1-bit stores? *)
+            and lsb = 0xffL in
             (* Break the value down to bytes *)
             let rec get_bytes offset (v,pos,vals) =
               if offset = n then (v,pos,vals)
               else 
-                let index = BinOp (PLUS,arr,Int(Int64.of_int offset, Reg 64)) in
-                let ind = eval_expr (delta,mu,index) in
+                let index = BinOp (PLUS,start_index,Int(Int64.of_int offset, Reg 64)) in
+                let ind = unwrap_val (eval_expr (delta,index)) in
                 let byte = BinOp (AND,v,Int(lsb,Reg 64)) in
-                let ebyte = eval_expr (delta,mu,byte) in
+                let ebyte = unwrap_val (eval_expr (delta,byte)) in
                 let v' = (BinOp (RSHIFT,v,Int(8L,Reg 64))) in
                 get_bytes (offset+1) (v',ind::pos,ebyte::vals)
             in
-            let _,poss,vals = get_bytes 0 (v3,[],[]) in
+            let _,poss,vals = get_bytes 0 (unwrap_val v3,[],[]) in
             (* Changing the indices based on endianness *)
-            let poss = if v4 = exp_false then poss else List.rev poss in
-            List.iter2 (fun pos value -> context_update mu pos value) poss vals ;
-            v3
+            let poss = if v4 = Val exp_false then poss else List.rev poss in
+            List.iter2 (fun pos value -> context_update memory pos value) poss vals ;
+            Mem memory
           | _ -> failwith "not a storable type"
         ) 
-      | Cast _ 
-      | Unknown _ -> exp_false
+      | Unknown _ -> Val exp_false
     in 
-    eval expr
+      eval expr
 
 
 (* The statement evaluation is practically a transition: *
  * (Delta,Mu,pc,stmt) -> (Delta',Mu',pc',stmt')          *
  * The contexts Sigma, Lambda remain unchanged during    *
  * transitions, but that can be easily changed           *)
-  let rec eval_stmt (delta,mu,sigma,lambda,pc,stmt,halt) = 
+  let rec eval_stmt (delta,sigma,lambda,pc,stmt,halt) = 
     let get_label e =
-      let v = eval_expr (delta,mu,e) in
-      match lab_of_exp v with
+      let v = eval_expr (delta,e) in
+      match lab_of_exp (unwrap_val v) with
              | None -> failwith "not a valid label"
              | Some lab -> label_decode lambda lab
     in
     let eval = function
       | Move (v,e,_) ->
-         let ev = eval_expr (delta,mu,e) in
+         let ev = eval_expr (delta,e) in
          context_update delta v ev ;
          let pc' = Int64.succ pc in
-         (delta,mu,pc',halt)
+         (delta,pc',halt)
       | Halt (_, _) -> 
       (* we don't care about the return value for the time being *)
-             (delta,mu,pc,true)
+             (delta,pc,true)
       | Jmp (e,_) -> 
              let pc' = get_label e in
-             (delta,mu,pc',halt) ;
+             (delta,pc',halt) ;
       | CJmp (b,e1,e2,_) ->
-          let pc' = match eval_expr (delta,mu,b) with
-            | v when v = exp_true -> get_label e1
-            | v when v = exp_false -> get_label e2
+          let pc' = match eval_expr (delta,b) with
+            | Val v when v = exp_true -> get_label e1
+            | Val v when v = exp_false -> get_label e2
             | _ -> failwith "not a boolean condition"
           in
-            (delta,mu,pc',halt)
+            (delta,pc',halt)
       | Assert (e,_) ->
-        let v = eval_expr (delta,mu,e) in
-        if v = exp_false then failwith ("assertion failed "^(Int64.to_string pc))
-        else (delta,mu,Int64.succ pc,halt)
-      | _ -> (delta,mu,Int64.succ pc,halt)
+        let v = eval_expr (delta,e) in
+        if v = Val exp_false then failwith ("assertion failed "^(Int64.to_string pc))
+        else (delta,Int64.succ pc,halt)
+      | Comment _ -> (delta,Int64.succ pc,halt)
+      | Special _ -> failwith "Specials not handled yet!"
     in
-    let (delta',mu',pc',halt) as c = eval stmt in
+    let (delta',pc',halt) as c = 
+      try eval stmt with Failure str -> raise (ExcState (str, pc))
+    in
      if halt then c
      else 
       let stmt' = inst_fetch sigma pc' in
-        eval_stmt (delta',mu',sigma,lambda,pc',stmt',halt)
+        eval_stmt (delta',sigma,lambda,pc',stmt',halt)
 
 (* Symbol meanings:                              *
  * pc: the program counter                       *
  * Sigma: mapping the program counter to a stmt  *
  * Lambda: mapping a label to a program counter  *
- * Delta: mapping a variable to a value          *
- * Mu: mapping a memory index to a value         *)
+ * Delta: mapping a variable to a value          *)
   let eval_ast_program stmts =
     let sigma : (addr, instr) Hashtbl.t = Hashtbl.create 5700 
     and pc = Int64.zero 
     and delta : (Ast.var, varval) Hashtbl.t = Hashtbl.create 5700
-    and mu : (Ast.exp, Ast.exp) Hashtbl.t = Hashtbl.create 5700
     and lambda : (label_kind, addr) Hashtbl.t = Hashtbl.create 5700 in
     (* Initializing Sigma and Lambda *)
     ignore 
@@ -219,7 +274,11 @@
           Int64.succ pc
         ) pc stmts ) ; 
     let init = inst_fetch sigma pc in
-    ignore (eval_stmt (delta,mu,sigma,lambda,pc,init,false)) ;
-    print_values delta ;
-    print_mem mu
+    (try ignore (eval_stmt (delta,sigma,lambda,pc,init,false)) 
+     with ExcState (s, pc) -> 
+      (prerr_endline ("Evaluation aborted at line "^(Int64.to_string pc));
+       prerr_endline ("reason: "^s))
+    );
+    print_values delta;
+    print_mem delta
 
