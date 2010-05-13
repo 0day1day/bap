5fdf97dfe7e1d436a7ffa9b58e743cb54e4c02e2
Edward J. Schwartz <edmcman@cmu.edu>
2010-05-13 19:58:23 +0000

    Fix some stupid bugs, and... now spawns 8 workers.
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@3986 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/stpexec.ml
+++ ocaml/stpexec.ml
@@ -4,7 +4,8 @@
    This is a pretty big hack, and is not portable in any shape or way.
 *)
 
-exception Alarm_signal;;
+exception Alarm_signal_internal;;
+exception Alarm_signal of int;;
 
 module D = Debug.Make(struct let name = "Stpexec" and default=`Debug end)
 open D
@@ -13,22 +14,94 @@ open Unix
 
 type result = Valid | Invalid | StpError | Timeout
 
+let select_timeout = 0.1 (* seconds *)
+
 let alarm_handler i =
-  raise Alarm_signal
+  raise Alarm_signal_internal
+
+(** Convert space delimited command string (command arg1 arg2 ... ) to a command and argument array *)
+let split_cmdstr cmdstr =
+  let slist = ExtString.String.nsplit cmdstr " " in
+  let cmd = List.hd slist in
+  let args = Array.of_list (List.tl slist) in
+  cmd, args
 
 let syscall cmd =
   let () = Sys.set_signal Sys.sigalrm (Sys.Signal_handle alarm_handler) in
-  let ic, oc, ec = Unix.open_process_full cmd (Unix.environment ()) in
-  let obuf = Buffer.create 16 in
-  let ebuf = Buffer.create 16 in
+  let (stdoutread,stdoutwrite) = pipe () in
+  let (stderrread,stderrwrite) = pipe () in
+  let (stdinread,stdinwrite) = pipe () in
+(*   let ic = in_channel_of_descr stdoutread in *)
+(*   let ec = in_channel_of_descr stderrread in *)
+  let cmdname, args = split_cmdstr cmd in
+  let pid = create_process cmdname args stdinread stdoutwrite stderrwrite in
   (try
-     while true do Buffer.add_channel obuf ic 1 done
-   with End_of_file -> ());
-  (try
-     while true do Buffer.add_channel ebuf ec 1 done
-   with End_of_file -> ());
-  let estatus = Unix.close_process_full (ic, oc, ec) in
-  (Buffer.contents obuf), (Buffer.contents ebuf), estatus
+     let obuf = Buffer.create 16 in
+     let ebuf = Buffer.create 16 in
+     let fdlist = [stdoutread; stderrread] in
+     List.iter (fun fd -> set_nonblock fd) fdlist;
+     let wait = ref true in
+     let estatus = ref None in
+     let buf = String.create 1 in
+     while !wait do    
+       
+       (* Read any new data from stdout/stderr *)
+       let rd () =
+	 let rl,_,_ = select fdlist [] fdlist select_timeout in
+	 List.iter
+	   (fun fd ->
+	      let buffer = if fd=stdoutread then obuf else ebuf in
+	      try
+		while true do
+		  (match read fd buf 0 1 with
+		   | 1 ->
+		       Buffer.add_string buffer buf
+		   | _ ->
+		       failwith "Assertion error");
+		done
+	      with Unix_error _ -> ()
+	   ) rl;
+(* 	 dprintf "Hmm: %d %d" (List.length rl) (List.length el); *)
+       in
+
+       (* Do a read *)
+       rd ();
+       
+       (* Check if the process is dead yet *)
+       let pid',estatus' = waitpid [WNOHANG] pid in
+       if pid' = pid then begin
+	 wait := false;
+	 estatus := Some(estatus')
+       end;
+       
+       (* Do another read, in case the process died *)
+       rd();
+       
+     done;
+     close stdoutread;
+     close stdoutwrite;
+     close stderrread;
+     close stderrwrite;
+     close stdinread;
+     close stdinwrite;
+     (Buffer.contents obuf), (Buffer.contents ebuf), Util.option_unwrap !estatus
+   with Alarm_signal_internal ->
+     raise (Alarm_signal pid))
+
+
+(* let syscall' cmd = *)
+(*   let () = Sys.set_signal Sys.sigalrm (Sys.Signal_handle alarm_handler) in *)
+(*   let ic, oc, ec = Unix.open_process_full cmd (Unix.environment ()) in *)
+(*   let obuf = Buffer.create 16 in *)
+(*   let ebuf = Buffer.create 16 in *)
+(*   (try *)
+(*      while true do Buffer.add_channel obuf ic 1 done *)
+(*    with End_of_file -> ()); *)
+(*   (try *)
+(*      while true do Buffer.add_channel ebuf ec 1 done *)
+(*    with End_of_file -> ()); *)
+(*   let estatus = Unix.close_process_full (ic, oc, ec) in *)
+(*   (Buffer.contents obuf), (Buffer.contents ebuf), estatus *)
  
 let result_to_string result =
   match result with
@@ -48,6 +121,8 @@ let runstp ?(timeout=60) file =
       let cmdstr = Printf.sprintf "cvc3 +unknown-check-model %s" file in
       let stpout,stperr,pstatus = syscall cmdstr
       in
+
+      dprintf "ok: %s %s" stpout stperr;
       
       (* Turn the alarm off *)
       ignore(alarm 0);
@@ -67,7 +142,7 @@ let runstp ?(timeout=60) file =
       let isinvalid = ExtString.String.exists stpout "Invalid." in
       let isvalid = ExtString.String.exists stpout "Valid." in
       
-      (*   dprintf "fail: %b %b %b" fail isinvalid isvalid; *)
+      dprintf "fail: %b %b %b" fail isinvalid isvalid;
       
       if isvalid then
 	Valid
@@ -81,9 +156,10 @@ let runstp ?(timeout=60) file =
 	failwith "Something weird happened."
     )
   with
-    Alarm_signal -> 
+    Alarm_signal(pid) -> 
       (* Another HUGE hack *)
-      ignore(Unix.system "killall -9 stp; killall -9 cvc3");      
+(*       ignore(Unix.system "killall -9 stp; killall -9 cvc3");       *)
+      kill pid 9;
       Timeout ;;
 
 (* let runstp ?(timeout=60) file = *)
