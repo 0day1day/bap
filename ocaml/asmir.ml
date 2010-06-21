41405a3b9f50dc5fa97c79ead2121533a91ef8cb
Edward J. Schwartz <edmcman@cmu.edu>
2010-06-21 02:33:43 +0000

    * change symbolic consts to better represent that they are also used implicitly for allocation * change the printing system from modules to classes, since we will want to create dynamic printers based on ELF information * add ability to look up symbols via elf table
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@4040 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/asmir.ml
+++ ocaml/asmir.ml
@@ -452,9 +452,28 @@ let get_symbols_hash ?(all=false) p =
 
 let (<<) = (lsl)
 
+(* bfd defs *)
+
 let bsf_debugging = 1 << 2
 let bsf_function = 1 << 3
 
+(** no flags *)
+let bsec_no_flags = 0x000
+(** allocate space when loading *)
+let bsec_alloc = 0x001
+(** load the section during loading *)
+let bsec_load = 0x002
+(** section has reloc info *)
+let bsec_reloc = 0x004
+(** read only *)
+let bsec_readonly = 0x008
+(** code *)
+let bsec_code = 0x010
+(** data *)
+let bsec_data = 0x020
+(** ROM *)
+let bsec_rom = 0x040
+(* others .... *)
 
 let get_function_ranges p =
   let symb = get_symbols p in
