8a43caa13cb467a84a4687cd6404fe3f3243da9e
Edward J. Schwartz <edmcman@cmu.edu>
2010-08-23 20:08:30 +0000

    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@4207 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/debug_snippets.ml
+++ ocaml/debug_snippets.ml
@@ -4,16 +4,19 @@
 
 (** AST debugging visitor *)
 
-let print_ast p =
-  let v = object(self)
-    inherit Ast_visitor.nop
-      (* Add each variable to definedvars *)
-    method visit_stmt stmt =
-      Printf.fprintf stderr "Stmt: %s\n" (Pp.ast_stmt_to_string stmt);
-      `SkipChildren
-  end 
-  in
+let v = object(self)
+  inherit Ast_visitor.nop
+    (* Add each variable to definedvars *)
+  method visit_stmt stmt =
+    Printf.fprintf stderr "Stmt: %s\n" (Pp.ast_stmt_to_string stmt);
+    `SkipChildren
+end 
+  
+let print_astcfg p =
   ignore(Ast_visitor.cfg_accept v p)
+
+let print_ast p =
+  ignore(Ast_visitor.prog_accept v p)
 
 let intv_to_string (i,t) =
   let e = Ast.Int(i,t) in
