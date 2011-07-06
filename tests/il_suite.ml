8291511aa008d3185c568b14c85d4f3df3c92ee7
Spencer Whitman <swhitman>
2011-07-06 19:31:55 +0000

    Checkpoint.
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/unittest@4819 f55ac123-dd1f-49ad-9780-241693a2721e

--- tests/il_suite.ml
+++ tests/il_suite.ml
@@ -4,12 +4,18 @@ open OUnit
 let test_input_fails _ =
   assert_raises (Arg.Bad("No input specified")) (fun _ -> Input.get_program());;
 
-let test_input () = todo "test_input not implemented yet";;
-
+let test_input_nop () = 
+  let p = Asmir.open_program "./x86/nop" in
+  let stmts = Asmir.asmprogram_to_bap p in
+  match stmts with
+	| stmt::stmts -> 
+	  todo ("First stmt of program ./x86/nop is "^Pp.ast_stmt_to_string(stmt))
+	| _ -> assert_failure 
+	  "asmprogram_to_bap of open_program \"./x86/nop\" returned an empty list!"
 
 
 let suite = "IL OUnit test suite" >::: 
   [
 	"test_input_fails" >:: test_input_fails;
-	"test_input" >:: test_input;
+	"test_input_nop" >:: test_input_nop;
   ]
