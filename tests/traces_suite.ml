c33ae25fe24b5e437a780a1295845dddb24abb93
Spencer Whitman <swhitman>
2011-07-21 19:45:20 +0000

    - Start converting tests/inject_halt.pl to utils/insert_halt.ml. - First pass at concrete execution test from examples in documentation. - Inlcude pcre library: re-entrant perl like regular expressions.
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/traces+unittest@4874 f55ac123-dd1f-49ad-9780-241693a2721e

--- tests/traces_suite.ml
+++ tests/traces_suite.ml
@@ -1,20 +1,59 @@
 open OUnit
+open Pcre
+
+let halted_file = "C/test.il.halted";;
+
+let rec find_fun ranges name = match ranges with
+  | [] -> assert_failure ("Could not find function "^name)
+  | (n,s,e)::rs -> if (n = name) then (s,e) else find_fun rs name
+
+let check_answer e = 
+  match e with
+  | Ast.Int(i,_) -> if (i <> Int64.of_int(42))
+	then 
+	  assert_failure ("Final value does not = 42, but " ^ (Int64.to_string i))
+	else ()
+  | _ -> assert_failure ("Final value is not an int!")
 
 let concrete_eval_setup _ =
-  let out = open_out "C/test.il" in
-  let pp = new Pp.pp_oc out in
-  let ast = Asmir.open_program ~loud:false "C/test" in
-  let prog = Asmir.asmprogram_to_bap ast in
-  pp#ast_program prog;
-  pp#close;
+  (*let out = open_out halted_file in
+  let pp = new Pp.pp_oc out in*)
+  let prog = Asmir.open_program ~loud:false "C/test" in
+  (* Find the start and end addresses of main *)
+  let ranges = Asmir.get_function_ranges prog in
+  let (start_addr,end_addr) = find_fun ranges "main" in
+  (*let ir = Asmir.asmprogram_to_bap prog in*)
+
+  Printf.printf "%s\t0x%Lx 0x%Lx\n" "main" start_addr end_addr;
+
+  (* 1. Find last ret in main's range *)
+  (* 2. Inject halt true after ret *)
+
+  (*pp#ast_program ir;
+  pp#close;*)
+
   assert_command ~verbose:true "./inject_halt.pl" [];
-  prog;;
+  start_addr;;
 
-let concrete_eval_tear_down _ = ();;
+let concrete_eval_tear_down _ = Sys.remove halted_file;;
 
-let concrete_eval_test ast = 
-  let prog = Parser.program_from_file "C/test.il.halted" in
-  prog;;
+let concrete_eval_test start_addr = 
+  let prog = Parser.program_from_file halted_file in
+  let ctx = Symbeval.concretely_execute prog ~s:start_addr in
+  let pat = "R_EAX_" in
+	Var.VarHash.iter 
+	  (fun k v ->
+		match k,v with
+		| var,Symbeval.Symbolic e ->
+		  if (pmatch ~pat (Pp.var_to_string var)) 
+		  then 
+			((Printf.printf 
+				"%s\n" 
+				((Pp.var_to_string var) ^ " = " ^ (Pp.ast_exp_to_string e)));
+			 check_answer e)
+		  else ()
+		| _ -> ()
+       ) ctx.Symbeval.delta;;
 
 let suite = "Traces" >:::
   [
--- utils/insert_halt.ml
+++ utils/insert_halt.ml
@@ -1,5 +1,7 @@
 let usage = "Usage: "^Sys.argv.(0)^" <options>\nTry to insert \"halt true\" into a BAP IL file."
 
+open Pcre
+
 let in_il = ref None
 and out_il = ref None
 and ret_addr = ref None
@@ -12,10 +14,19 @@ let speclist =
 	("-out",
 	 Arg.String(fun s -> out_il := Some(s)),
 	 "<file> Output IL file (copy of -in <file> with \"halt true\" added)");
-	("-addr",
+	(*("-addr",
 	 Arg.String(fun s -> ret_addr := Some(Int64.of_string s)),
-	 "<file> Input IL file to add \"halt true\" to");	
+	 "<0xADDRESS> ADDRESS where the main function starts.");*)	
   ];;
 
 let anon x = raise(Arg.Bad("Unexpected argument: '"^x^"'"));;
 let () = Arg.parse speclist anon usage;;
+
+(**
+  1. Find & print out line which occurs before where we want to put halt true 
+  2. Output new file with "halt true" in proper location
+  3. Return address of Main (begining of execution point)
+**)
+
+(* Find start and end addresses of main *)
+let p = Asmir.open_program
