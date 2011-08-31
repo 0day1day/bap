85a6205b30f4796f46dee6f332a41756ae2b1352
Thanassis Avgerinos <thanassis@cmu.edu>
2011-08-31 03:49:25 +0000

    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5047 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/ast.ml
+++ ocaml/ast.ml
@@ -70,6 +70,7 @@ and reg_8 = Reg 8
 and reg_16 = Reg 16
 and reg_32 = Reg 32
 and reg_64 = Reg 64
+and reg_128 = Reg 128
 
 (** False constant. (If convenient, refer to this rather than building your own.) *)
 let exp_false = Int(bi0, reg_1)
