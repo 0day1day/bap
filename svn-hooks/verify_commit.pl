efe57faa2035a092372fb46b397a3b280b413872
Spencer Whitman <swhitman>
2011-09-15 15:49:10 +0000

    -Change name so that current post-commit can call this script instead of replace it -Add opt-out switch, and opt-out if commit doesn't touch trunk (untested)
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5159 f55ac123-dd1f-49ad-9780-241693a2721e

--- svn-hooks/verify_commit.pl
+++ svn-hooks/verify_commit.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use strict;
+use Getopt::Long;
 # apt-get install libsvn-hooks-perl or libsvn-svnlook-perl
 use SVN::Core;
 use SVN::Client;
@@ -7,11 +9,21 @@ use Term::ReadKey;
 use Capture::Tiny qw/tee_merged/;  # requires package libcapture-tiny-perl 
 $| = 1;
 
-$rep_url = 'https://kestrel.ece.cmu.edu/svn/bap/trunk';
-$path = "/tmp/test-svn-hook";
-$true = 1;
-$curr_dir = `pwd`;
+my $rep_url = 'https://kestrel.ece.cmu.edu/svn/bap/trunk';
+my $local_path = "/tmp/test-svn-hook";
+my $true = 1;
+my $curr_dir = `pwd`;
+my $svn_look = '/usr/bin/svnlook';
+my ($revision, $repos, $svnlook);
+my $getpin = 0;
+my $merged;
 
+GetOptions(
+    'revision|r=s'    => \$revision,
+    'repos=s'         => \$repos,
+    'svnlook=s'       => \$svnlook,
+    'getpin'          => \$getpin,
+    );
 #
 # Keep track of the last 10 lines to print out in case of error; Exit on error
 #
@@ -34,6 +46,9 @@ sub check_system {
     }
 }
 
+#
+# Simple prompt to get svn authentication for checkout; for testing.
+#
 sub simple_prompt {
     my $cred = shift;
     my $realm = shift;
@@ -55,9 +70,15 @@ sub simple_prompt {
     print "\n";
 }
 
+# Did user provide opt-out message?
+exit (0) if(`$svnlook log -r $revision $repos` =~ /unittest-opt-out/);
+
+# Make sure this revision touches trunk
+exit (0) if(`$svnlook changed -r $revision $repos` !~ /[U|A|D]\s+trunk.*$/);
+
 # check out repository to /tmp
-mkdir $path;
-chdir $path;
+mkdir $local_path;
+chdir $local_path;
 
 my $ctx = new SVN::Client(
     auth => [SVN::Client::get_simple_provider(),
@@ -67,7 +88,7 @@ my $ctx = new SVN::Client(
 
 print "Checking out repository $rep_url\n";
 
-$ctx->checkout($rep_url, $path, 'HEAD', $true);
+$ctx->checkout($rep_url, $local_path, $revision, $true);
 
 print "Finished checking out repository $rep_url\n";
 
@@ -77,17 +98,19 @@ check_system("./autogen.sh");
 check_system("./configure", "CFLAGS=-DAMD64");
 
 # getpin?
-print "Getting pin\n";
-chdir "pintraces";
-check_system("./getpin.sh");
-chdir "../";
+if($getpin) {
+    print "Getting pin\n";
+    chdir "pintraces";
+    check_system("./getpin.sh");
+    chdir "../";
+}
 
 # make test
 print "Making test\n";
-check_system("make","t");
+check_system("make");
 
 # go back to where we started
 chdir $curr_dir;
 # remove /tmp/trunk
-print "Removing temporary repository at $path\n";
-check_system('rm', '-rf', $path)
+print "Removing temporary repository at $local_path\n";
+check_system('rm', '-rf', $local_path)
