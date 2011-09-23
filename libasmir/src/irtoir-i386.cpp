c4102d4978af233e73dc0d5155cd32305070a177
Sangkil Cha <sangkilc@andrew.cmu.edu>
2011-09-23 20:04:19 +0000

    syscall special reg handling
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5264 f55ac123-dd1f-49ad-9780-241693a2721e

--- libasmir/src/irtoir-i386.cpp
+++ libasmir/src/irtoir-i386.cpp
@@ -62,6 +62,8 @@
 #define OFFB_TILEN     offsetof(VexGuestX86State,guest_TILEN)
 #define OFFB_NRADDR    offsetof(VexGuestX86State,guest_NRADDR)
 
+#define OFFB_IP_AT_SYSCALL offsetof(VexGuestX86State,guest_IP_AT_SYSCALL)
+
 //
 // Sub register offsets, calculated manually
 //
@@ -389,7 +391,9 @@ static string reg_offset_to_name( int offset )
         case OFFB_TILEN:    name = "TILEN";     break;
         case OFFB_NRADDR:   name = "NRADDR";    break;
 
-        default:            
+        case OFFB_IP_AT_SYSCALL: name = "IP_AT_SYSCALL"; break;
+
+        default:
             panic("Unrecognized register name");
     }
 
