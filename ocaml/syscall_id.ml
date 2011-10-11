41949c79f36abbefc9f9493162dd3c3ca8f860a0
Edward J. Schwartz <edmcman@cmu.edu>
2011-10-11 21:41:49 +0000

    We're getting warmer....
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/traces+merging@5391 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/syscall_id.ml
+++ ocaml/syscall_id.ml
@@ -1,6 +1,10 @@
 (** Use dataflow analysis to statically identify system call types
 
-    XXX: The lattice is not correct yet.
+    The lattice is a map from each program location to Top | FromSC |
+    Syscall | Bottom.  Executing a system call generates
+    FromSC. Writing to %eax will convert any addresses at FromSC to
+    Syscall.  Writing to %eax for any other state will go to
+    Bottom. We also keep track of termination separately.
 
     @author ejs
 *)
@@ -14,42 +18,75 @@ open D
 
 let syscall_reg = Disasm_i386.eax;;
 
+module AddrMap = Cfg.BM
+
 module DFSPEC = struct
   module G = Cfg.AST.G
   module L = struct
-    type t = Top | Term | FromSyscall | Syscall of (big_int * Type.typ) | Bottom
-    let top = Top (* Normal blocks are not system calls *)
-    let equal x y = match x, y with
+    type littlet = Top | FromSyscall | Syscall of (big_int * Type.typ) | Bottom
+    type termt = Term | NoTerm
+    type t = termt * littlet AddrMap.t
+    let top = NoTerm, AddrMap.empty
+    let equal_littlet x y = match x, y with
       | Top, Top -> true
-      | Term, Term -> true
       | FromSyscall, FromSyscall -> true
       | Syscall(bi1, t1), Syscall(bi2, t2) when bi1 ==% bi2 && t1 = t2 -> true
       | Bottom, Bottom -> true
       | _ -> false
-    let meet x y = match x,y with
-      | Top, o -> o
-      | o, Top -> o
-      | Bottom, _ -> Bottom
-      | _, Bottom -> Bottom
-      | Term, FromSyscall -> FromSyscall
-      | FromSyscall, Term -> FromSyscall
-      | x, y when equal x y -> x
-      | _, _ -> Bottom
+    let equal x y = 
+      let equal_term = (=) in
+      let equal_map x y =
+        AddrMap.equal equal_littlet x y
+      in
+      (equal_term (fst x) (fst y)) && (equal_map (snd x) (snd y))
+
+    let meet_littlet x y = match x, y with
+      | FromSyscall, (Syscall _ as sc) -> sc
+      | (Syscall _ as sc), FromSyscall -> sc
+      | x, y when equal_littlet x y -> x
+      | Top, x -> x
+      | x, Top -> x
+      | _ -> Bottom
+
+    let meet x y =
+      let meet_term x y = if x = y then x else NoTerm in
+      let meet_map x y =
+        AddrMap.fold
+          (fun k yv xmap ->
+            let xv = try AddrMap.find k xmap
+              with Not_found -> Top in
+            AddrMap.add k (meet_littlet xv yv) xmap
+          ) y x
+      in
+      meet_term (fst x) (fst y), meet_map (snd x) (snd y)
+
   end
 
   let transfer_function g n init =
     let stmts = Cfg.AST.get_stmts g n in
     let process_stmt a s = match s,a with
-      | Move(v, Int(i, t), _), L.FromSyscall when v=syscall_reg -> L.Syscall(i, t)
-      | Move(v, _, _), _ when v=syscall_reg -> L.meet a L.Term
-      | s, L.Term when Ast.is_syscall s -> L.FromSyscall
+      | Move(v, Int(i, t), _), (L.Term, m) when v=syscall_reg ->
+        (* Meet all addresses with Syscall(i, t) *)
+        let newv = L.Syscall(i, t) in
+        L.Term, AddrMap.map (fun v -> L.meet_littlet newv v) m
+      | Move(v, _, _), (L.Term, m) when v=syscall_reg ->
+        (* Meet all addresses with Bottom *)
+        let newv = L.Bottom in
+        L.Term, AddrMap.map (fun v -> L.meet_littlet newv v) m
+      | s, (L.Term, m) when Ast.is_syscall s ->
+      (* Meet all existing addresses with bottom, and then meet
+         address a with FromSyscall *)
+        let newv = L.Bottom in
+        let newm = AddrMap.map (fun v -> L.meet_littlet newv v) m in
+        let addr = Cfg.AST.G.V.label n in
+        L.Term, AddrMap.add addr L.FromSyscall newm
       | _, _ -> a
     in
-    (* We don't want to propagate Syscall values across blocks *)
-    let init = match init with
-      | L.Syscall _ -> L.Term
-      | _ -> init
-    in
+    (* (\* We don't want to propagate Syscall values across blocks *\) *)
+    (* let init = match init with *)
+    (*   | L.Syscall _ -> L.Term *)
+    (*   | _ -> init *)
+    (* in *)
     List.fold_left process_stmt init stmts
   let s0 g =
     (* let check_v v a = *)
@@ -59,7 +96,7 @@ module DFSPEC = struct
     (* in *)
     (* Cfg.AST.G.fold_vertex check_v g [snd(Cfg_ast.find_error g)] *)
     snd(Cfg_ast.find_exit g)
-  let init _ = L.Term
+  let init _ = L.Term, AddrMap.empty
   let dir = GraphDataflow.Backward
 
 end
@@ -67,17 +104,18 @@ end
 module DF = GraphDataflow.Make(DFSPEC)
 
 let find_syscalls g =
-  let inh, out = DF.worklist_iterate g in
+  let _inh, _out = DF.worklist_iterate g in
   let process_vertex n a =
-    match out n with
-    | DFSPEC.L.Syscall(i, t) ->
-      let i = int_of_big_int i in
-      dprintf "Found syscall %d! yay" i;
-      if debug then (
-        let stmts = Cfg.AST.get_stmts g n in
-        Debug_snippets.print_ast stmts;
-      );
-      (n, i)::a
-    | _ -> a
+    (* match out n with *)
+    (* | DFSPEC.L.Syscall(i, t) -> *)
+    (*   let i = int_of_big_int i in *)
+    (*   dprintf "Found syscall %d! yay" i; *)
+    (*   if debug then ( *)
+    (*     let stmts = Cfg.AST.get_stmts g n in *)
+    (*     Debug_snippets.print_ast stmts; *)
+    (*   ); *)
+    (*   (n, i)::a *)
+    (* | _ -> a *)
+    a
   in
   Cfg.AST.G.fold_vertex process_vertex g []
