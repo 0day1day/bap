b5c19648e92e14385b0f30e9c20cc2a2d02210e8
Spencer Whitman <swhitman>
2011-10-28 15:31:15 +0000

    Changes to reflect new format of reporting errors.
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5465 f55ac123-dd1f-49ad-9780-241693a2721e

--- tests/analyze_test_log.ml
+++ tests/analyze_test_log.ml
@@ -1,4 +1,5 @@
 open Pcre
+open BatListFull
 
 let usage = "Usage: "^Sys.argv.(0)^" <-l logfile> [-o output]\n\
              Collect statistics for a logfile from the long test.";;
@@ -7,8 +8,8 @@ let log = ref stdin;;
 let out = ref stdout;;
 let verbose = ref false;;
 
-let bap_unknown_tbl = Hashtbl.create 100;;
-let expanded_hashtbl = Hashtbl.create 100;;
+let bap_unknown_hashtbl = Hashtbl.create 100;;
+let probably_right_hashtbl = Hashtbl.create 100;;
 let small_hashtbl = Hashtbl.create 100;;
 
 let speclist =
@@ -33,8 +34,9 @@ let print_hashtbl table =
   let print_item (key,value) = 
 	print_out (key^" : "^(string_of_int value))
   in
-  let cmp (_,value1) (_,value2) = compare value1 value2 in
-  List.map print_item (List.sort cmp hash_list);;
+  (* Print list in descending order *)
+  let cmp (_,value1) (_,value2) = compare value2 value1 in
+  ignore(List.map print_item (List.sort ~cmp hash_list));;
 
 
 let insert_item hashtbl item =
@@ -45,62 +47,116 @@ let insert_item hashtbl item =
 
 let bap_regexp = regexp "^AsmirTest: BAP unknown disasm_instr \S+: disasm_i386: unimplemented feature: unsupported opcode: (.*)$";;
 let vex_regexp = regexp "^vex x86->IR: unhandled instruction bytes: (.*)$";;
-let trace_eval_regexp = regexp "^WARNING \(TraceEval\): Difference between BAP and trace values in .*asm (\".*?\") .*context (.*)\]: (R_\S*) Trace=(\S*) Eval=(\S*)$";;
+let trace_eval_regexp = regexp "^WARNING \(TraceEval\): Difference between BAP and trace values for \[\*(R_\S*)\* Trace=(\S*) Eval=(\S*)\]";;
+let trace_eval_regexp2 = regexp "^WARNING \(TraceEval\): This is due to one of the following statments:";;
+let stmt_regexp = regexp "^\{addr .*asm (\".*?\") (.*)\}$";;
 let asm_regexp = regexp "^\"(.*)\"";;
 let first_word_regexp = regexp "^(\S*)";;
-
+let special_regexp = regexp "(int|sysenter)";;
 
 let get_match rex text = 
   let matches = extract ~rex ~full_match:false text in
   Array.get matches 0;;
 
+let wrong_register = ref None;;
+let asm_buff = ref [];;
+
+(* Process list sorts asm instructions into def problem, and might be problems 
+   ignoring "bad" instructions like syscalls *)
+let process_list asms =
+  if (List.exists (fun (asm,_) -> pmatch ~rex:special_regexp asm) asms)
+  then (
+    List.map 
+      (fun (asm,r) -> 
+	let asm = (get_match first_word_regexp asm)^" "^r in
+	insert_item probably_right_hashtbl asm;
+	if (!verbose) then (
+	  print_out "Probably got right, but just in case:";
+    	  print_out ("Asm = "^asm);
+	  (* print_out ("Context = "^Array.get stmts 1); *)
+	)
+      )
+      asms
+  )
+  else (
+    (* No special indicates these were instructions we definitely got wrong *)
+    List.map 
+      (fun (asm,r) -> 
+	let asm = (get_match first_word_regexp asm)^" "^r in
+	insert_item small_hashtbl asm;
+	if (!verbose) then (
+    	  print_out "Definitly got wrong:";
+    	  print_out ("Asm = "^asm);
+	  (* print_out ("Context = "^Array.get stmts 1); *)
+	)
+      )
+      asms
+  );;
+
 
 let process_line l = (
-  try
-	let matches = extract ~rex:bap_regexp ~full_match:false l in
-	let bap_instr = Array.get matches 0 in
-	insert_item bap_unknown_tbl bap_instr;
+  match !wrong_register with
+  | Some(r) -> (
+    (* Ignore the second warnning line *)
+    if (pmatch ~rex:trace_eval_regexp2 l) then () else
+      try
+	let stmts = extract ~rex:stmt_regexp ~full_match:false l in
+    	let asm = get_match asm_regexp (Array.get stmts 0) in
+	asm_buff := (asm,r)::(!asm_buff)
+      with Not_found -> 
+	process_list !asm_buff;
+	(* Reset counters as we are no longer analyzing the list of bad
+	   instructions *)
+	wrong_register := None;
+	asm_buff := [];
+  )
+  | None -> (
+    try
+      let matches = extract ~rex:bap_regexp ~full_match:false l in
+      let bap_instr = Array.get matches 0 in
+      insert_item bap_unknown_hashtbl bap_instr;
+    (*if (!verbose) then 
+      print_out ("Unknown BAP instructions: "^bap_instr);*)
+    with Not_found -> (
+      try
+	let matches = extract ~rex:vex_regexp ~full_match:false l in
+	let vex_instr = Array.get matches 0 in
+	insert_item small_hashtbl vex_instr;
 	if (!verbose) then 
-	  print_out ("Unknown BAP instructions: "^bap_instr);
-  with Not_found -> (
+	  print_out ("Unknown VEX instructions: "^vex_instr);
+      with Not_found -> (
 	try
-	  let matches = extract ~rex:vex_regexp ~full_match:false l in
-	  let vex_instr = Array.get matches 0 in
-	  insert_item small_hashtbl vex_instr;
-	  if (!verbose) then 
-		print_out ("Unknown VEX instructions: "^vex_instr);
-	with Not_found -> (
-	  try
-		let matches = extract ~rex:trace_eval_regexp ~full_match:false l in
-		let asm = get_match asm_regexp (Array.get matches 0) in 
-		insert_item small_hashtbl (get_match first_word_regexp asm);
-		if (!verbose) then (
-		  insert_item expanded_hashtbl asm;	  
-		  print_out "Second exp result:";
-		  print_out ("Asm = "^asm);
-		  print_out ("Context = "^Array.get matches 1);
-		  print_out ("Reg = "^Array.get matches 2);
-		  print_out ("Trace = "^Array.get matches 3);
-		  print_out ("Eval = "^Array.get matches 4);
-		)
-	  with Not_found ->  () (*print_out ("Unknown line: "^l)*)
-	)
+	  let matches =
+	    extract ~rex:trace_eval_regexp ~full_match:false l 
+	  in
+	  wrong_register := Some(Array.get matches 0);
+	  if (!verbose) then (
+	    print_out "The following belongs to the below lines:";
+	    print_out ("Reg = "^Array.get matches 0);
+	    print_out ("Trace = "^Array.get matches 1);
+	    print_out ("Eval = "^Array.get matches 2);
+	  )
+	with Not_found ->  () (*print_out ("Unknown line: "^l)*)
+      )
+    )
   )
 );;
-  
+
 
 let _ =
   (try
-	 while true do process_line (input_line !log) done
+     while true do process_line (input_line !log) done
    with End_of_file -> (close_in !log));
-  if (!verbose) then (
-	print_out "Expanded Summary:";
-	ignore(print_hashtbl expanded_hashtbl);
-	flush !out);
   print_out "BAP unknown Summary:";
-  print_hashtbl bap_unknown_tbl;
+  print_hashtbl bap_unknown_hashtbl;
   flush !out;
-  print_out "Small Summary:";
+  
+  print_out "Incorrect Implementation Summary:";
   print_hashtbl small_hashtbl;
+  flush !out;
+
+  print_out "Likely special attribution Summary:";
+  print_hashtbl probably_right_hashtbl;
+  flush !out;
+
   close_out !out;;
-	
