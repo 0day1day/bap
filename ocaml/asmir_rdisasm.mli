0902ebe1749f64f376f2cb5235e43f4de1ca29ea
Edward J. Schwartz <edmcman@cmu.edu>
2011-11-07 19:06:48 +0000

    Minor refinement to rdisasm
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5499 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/asmir_rdisasm.mli
+++ ocaml/asmir_rdisasm.mli
@@ -1,2 +1,8 @@
-val rdisasm_at : ?max:int -> Asmir.asmprogram -> int64 -> Ast.program * string
-val rdisasm : ?max:int -> Asmir.asmprogram -> Ast.program * string
+open Ast
+open Type
+
+type callback = addr -> addr -> stmt list -> bool
+
+val rdisasm_at : ?f:callback -> Asmir.asmprogram -> int64 -> Ast.program * string
+val rdisasm : ?f:callback -> Asmir.asmprogram -> Ast.program * string
+val max_callback : int -> callback
