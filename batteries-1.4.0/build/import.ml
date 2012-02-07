852df65ce56ab4ba165e399e0d1a9e73dcfd880d
Edward J. Schwartz <edmcman@cmu.edu>
2012-02-07 16:16:04 +0000

    * Add batteries 1.4.1
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@5719 f55ac123-dd1f-49ad-9780-241693a2721e

--- batteries-1.4.0/build/import.ml
+++
@@ -1,6 +0,0 @@
-(*
-  Placeholder for a future tool.
-
-  This tool will import a set of interfaces from a library for quick
-  initialization of layer code.
-*)
--- batteries-1.4.0/src/batteries.ml
+++
@@ -1,8 +0,0 @@
-include Batteries_uni (* the meat of this module is in batteries_uni.mli *)
-module Mutex = BatMutex;;
-module RMutex = BatRMutex;;
-
-Unix.lock := Mutex.Mutex.make ();;
-IO.lock := Mutex.Mutex.make ();;
-IO.lock_factory := Mutex.Mutex.make;;
-lock := Mutex.Mutex.make ();;
