d4421340165697e4354ed481de0cb15b3633f3b1
Edward J. Schwartz <edmcman@cmu.edu>
2012-02-11 20:39:44 +0000

    not done
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/traces+piqi@5751 f55ac123-dd1f-49ad-9780-241693a2721e

---
+++ libtrace/src/ocaml/utils/readtrace.ml
@@ -0,0 +1,18 @@
+open Trace_container
+open Frame_piqi_ext
+
+let print f =
+  print_frame f
+
+let print_all f =
+
+  let r = new reader f in
+
+  while not r#end_of_trace do
+    print r#get_frame
+  done;;
+
+let () =
+  if Array.Length Sys.argv = 2 then
+    print_all Sys.argv.(1);;
+
