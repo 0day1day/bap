3218aa56540d87f87aef3478a21c2fae22fcd91d
Edward J. Schwartz <edmcman@cmu.edu>
2012-08-09 20:36:36 +0000

    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/branches/cfg_recovery@6590 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml/copy_prop.mli
+++ ocaml/copy_prop.mli
@@ -4,4 +4,4 @@
 val copyprop_ssa : Cfg.SSA.G.t -> Ssa.exp Var.VarMap.t
 
 (** Compute copy propagation for AST CFGs *)
-val copyprop_ast : Cfg.AST.G.t -> Ast.exp Var.VarMap.t
+(* val copyprop_ast : Cfg.AST.G.t -> Ast.exp Var.VarMap.t *)
