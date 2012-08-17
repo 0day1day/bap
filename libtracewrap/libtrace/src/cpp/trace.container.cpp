c0a32d41a6e56b20039e3ace4b62e37f0ce32774
Edward J. Schwartz <edmcman@cmu.edu>
2012-08-17 23:48:59 +0000

    switch to FILEs to allow big traces on windows
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@6647 f55ac123-dd1f-49ad-9780-241693a2721e

--- libtracewrap/libtrace/src/cpp/trace.container.cpp
+++ libtracewrap/libtrace/src/cpp/trace.container.cpp
@@ -5,10 +5,14 @@
  */
 
 #include "trace.container.hpp"
+#include <stdio.h>
 #include <iostream>
+#include <string>
 
-#define WRITE(x) { ofs.write((const char *)(&(x)), sizeof(x)); }
-#define READ(x) { ifs.read((char *)(&(x)), sizeof(x)); }
+#define WRITE(x) { if (fwrite(&(x), sizeof(x), 1, ofs) != 1) { throw (TraceException("Unable to write to trace")); } }
+#define READ(x) { if (fread(&(x), sizeof(x), 1, ifs) != 1) { throw (TraceException("Unable to read from trace")); } }
+#define SEEK(f,x) { if (fseeko(f, x, SEEK_SET) != 0) { throw (TraceException("Unable to seek in trace to offset " + x)); } }
+#define TELL(f) ftello(f)
 
 namespace SerializedTrace {
 
@@ -16,17 +20,17 @@ namespace SerializedTrace {
                                              bfd_architecture arch,
                                              uint64_t machine,
                                              uint64_t frames_per_toc_entry_in,
-                                             bool auto_finish_in) throw (std::ofstream::failure, TraceException)
-    : ofs ( filename.c_str(), std::ios_base::binary | std::ios_base::out | std::ios_base::trunc ),
-      num_frames (0),
+                                             bool auto_finish_in) throw (TraceException)
+    : num_frames (0),
       frames_per_toc_entry (frames_per_toc_entry_in),
       arch (arch),
       mach (machine),
       auto_finish (auto_finish_in),
       is_finished (false)
   {
-    ofs.exceptions( std::ios_base::failbit | std::ios_base::badbit | std::ios_base::eofbit );
-    ofs.seekp(first_frame_offset);
+    ofs = fopen(filename.c_str(), "wb");
+    if (!ofs) { throw (TraceException("Unable to open trace file for writing")); }
+    SEEK(ofs, first_frame_offset);
   }
 
   TraceContainerWriter::~TraceContainerWriter(void) throw () {
@@ -37,18 +41,18 @@ namespace SerializedTrace {
       try {
         finish();
       }
-      catch (std::ofstream::failure e) {
+      catch (std::exception const &e) {
         std::cerr << "Exception " << e.what() << " occured during TraceContainerWriter's auto-finish" << std::endl;
       }
     }
   }
 
-  void TraceContainerWriter::add(frame &f) throw (std::ofstream::failure, TraceException) {
+  void TraceContainerWriter::add(frame &f) throw (TraceException) {
     /* Is is time for a toc entry? */
     if (num_frames > 0 && (num_frames % frames_per_toc_entry) == 0) {
       /* Yes.  Add the file offset where we will insert this frame to
          toc. */
-      toc.push_back(ofs.tellp());
+      toc.push_back(TELL(ofs));
     }
 
     num_frames++;
@@ -67,21 +71,29 @@ namespace SerializedTrace {
     WRITE(len);
 
     /* Write the frame. */
-    std::streampos old_offset = ofs.tellp();
+    off_t old_offset = TELL(ofs);
+    if (old_offset == -1) {
+      throw (TraceException("Unable to determine the current offset in the trace"));
+    }
 
-    ofs << s;
+    if (fwrite(s.c_str(), 1, len, ofs) != len) {
+      throw (TraceException("Unable to write frame to trace file"));
+    }
 
     /* Double-check our size. */
-    assert ((uint64_t)old_offset + len == (uint64_t)ofs.tellp());
+    assert ((uint64_t)old_offset + len == (uint64_t)TELL(ofs));
   }
 
-  void TraceContainerWriter::finish(void) throw (std::ofstream::failure,TraceException) {
+  void TraceContainerWriter::finish(void) throw (TraceException) {
     if (is_finished) {
       throw (TraceException("finish called twice"));
     }
 
     /* Save the offset where we will write the toc. */
-    uint64_t toc_offset = ofs.tellp();
+    uint64_t toc_offset = TELL(ofs);
+    if (toc_offset == -1) {
+      throw (TraceException("Unable to determine the current offset in the trace"));
+    }
 
     /* Make sure the toc is the right size. */
     assert ((num_frames == 0) || ((num_frames - 1) / frames_per_toc_entry) == toc.size());
@@ -98,32 +110,34 @@ namespace SerializedTrace {
        and the offset of field m at the start of the trace. */
 
     /* Magic number. */
-    ofs.seekp(magic_numer_offset);
+    SEEK(ofs, magic_number_offset);
     WRITE(magic_number);
 
     /* Trace version. */
-    ofs.seekp(trace_version_offset);
+    SEEK(ofs, trace_version_offset);
     WRITE(out_trace_version);
 
     /* CPU architecture. */
-    ofs.seekp(bfd_arch_offset);
+    SEEK(ofs, bfd_arch_offset);
     uint64_t archt = (uint64_t) arch;
     WRITE(archt);
 
     /* Machine type. */
-    ofs.seekp(bfd_machine_offset);
+    SEEK(ofs, bfd_machine_offset);
     WRITE(mach);
 
     /* Numer of trace frames */
-    ofs.seekp(num_trace_frames_offset);
+    SEEK(ofs, num_trace_frames_offset);
     WRITE(num_frames);
 
     /* Offset of toc. */
-    ofs.seekp(toc_offset_offset);
+    SEEK(ofs, toc_offset_offset);
     WRITE(toc_offset);
 
     /* Finally, close the file and mark us as finished. */
-    ofs.close();
+    if (fclose(ofs)) {
+      throw (TraceException("Unable to close trace file"));
+    }
     is_finished = true;
   }
 
@@ -131,10 +145,10 @@ namespace SerializedTrace {
     return is_finished;
   }
 
-  TraceContainerReader::TraceContainerReader(std::string filename) throw (std::ifstream::failure, TraceException)
-    : ifs ( filename.c_str(), std::ios_base::binary | std::ios_base::in )
+  TraceContainerReader::TraceContainerReader(std::string filename) throw (TraceException)
   {
-    ifs.exceptions( std::ios_base::failbit | std::ios_base::badbit | std::ios_base::eofbit );
+    ifs = fopen(filename.c_str(), "rb");
+    if (!ifs) { throw (TraceException("Unable to open trace for reading")); }
 
     /* Verify the magic number. */
     uint64_t magic_number_read;
@@ -163,22 +177,22 @@ namespace SerializedTrace {
     READ(toc_offset);
 
     /* Find the toc. */
-    ifs.seekg(toc_offset);
+    SEEK(ifs, toc_offset);
 
     /* Read number of frames per toc entry. */
     READ(frames_per_toc_entry);
 
     /* Read each toc entry. */
-    for (int i = 0; i < ((num_frames - 1)/ frames_per_toc_entry); i++) {
+    for (int i = 0; i < ((num_frames - 1) / frames_per_toc_entry); i++) {
       uint64_t offset;
       READ(offset);
       toc.push_back(offset);
     }
 
     /* We should be at the end of the file now. */
-    std::ifstream::off_type us = ifs.tellg();
-    ifs.seekg(0, std::ios_base::end);
-    if (us != ifs.tellg()) {
+    off_t us = TELL(ifs);
+    off_t end = fseeko(ifs, 0, SEEK_END);
+    if (us != TELL(ifs) || end != 0) {
       throw(TraceException("The table of contents is malformed."));
     }
 
@@ -219,30 +233,30 @@ namespace SerializedTrace {
 
     if (toc_number == 0) {
       current_frame = 0;
-      ifs.seekg(first_frame_offset);
+      SEEK(ifs, first_frame_offset);
     } else {
       current_frame = toc_number * frames_per_toc_entry;
       /* Use toc_number - 1 because there is no toc for frames [0,m). */
-      ifs.seekg(toc[toc_number - 1]);
+      SEEK(ifs, toc[toc_number - 1]);
     }
 
     while (current_frame != frame_number) {
       /* Read frame length and skip that far ahead. */
       uint64_t frame_len;
       READ(frame_len);
-      ifs.seekg((uint64_t)ifs.tellg() + frame_len);
+      SEEK(ifs, (uint64_t)TELL(ifs) + frame_len);
       current_frame++;
     }
   }
 
-  std::auto_ptr<frame> TraceContainerReader::get_frame(void) throw (std::ifstream::failure, TraceException) {
+  std::auto_ptr<frame> TraceContainerReader::get_frame(void) throw (TraceException) {
     /* Make sure we are in bounds. */
     check_end_of_trace("get_frame() on non-existant frame");
 
     uint64_t frame_len;
     READ(frame_len);
     if (frame_len == 0) {
-      throw (TraceException("Read zero-length frame at offset " + ifs.tellg()));
+      throw (TraceException("Read zero-length frame at offset " + TELL(ifs)));
     }
 
     /* We really just want a variable sized array, but MS VC++ doesn't support C99 yet.
@@ -252,7 +266,9 @@ namespace SerializedTrace {
     auto_vec<char> buf ( new char[frame_len] );
 
     /* Read the frame into buf. */
-    ifs.read(buf.get(), frame_len);
+    if (fread(buf.get(), 1, frame_len, ifs) != frame_len) {
+      throw (TraceException("Unable to read frame from trace"));
+    }
 
     std::string sbuf(buf.get(), frame_len);
 
@@ -265,7 +281,7 @@ namespace SerializedTrace {
     return f;
   }
 
-  std::auto_ptr<std::vector<frame> > TraceContainerReader::get_frames(uint64_t requested_frames) throw (std::ifstream::failure, TraceException) {
+  std::auto_ptr<std::vector<frame> > TraceContainerReader::get_frames(uint64_t requested_frames) throw (TraceException) {
     check_end_of_trace("get_frames() on non-existant frame");
 
     std::auto_ptr<std::vector<frame> > frames(new std::vector<frame>);
