4c82c765c889263d21968f0b5627c2d61e907b59
Edward J. Schwartz <edmcman@cmu.edu>
2012-08-31 14:21:06 +0000

    upgrade PIN version
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@6672 f55ac123-dd1f-49ad-9780-241693a2721e

--- pintraces/getpin.sh
+++ pintraces/getpin.sh
@@ -7,7 +7,7 @@ set -x
 
 # check if pin dir exists first
 
-wget 'http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.11-49306-gcc.3.4.6-ia32_intel64-linux.tar.gz' -O - | tar -xvz -C ..
+wget 'http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.12-53271-gcc.4.4.7-ia32_intel64-linux.tar.gz' -O - | tar -xvz -C ..
 rm -rf ../pin
 mv ../pin-* ../pin
 #make
