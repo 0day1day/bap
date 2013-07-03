5d002042baaaa7afd47ec01c41b1a40f2fe6791a
Edward J. Schwartz <edmcman@cmu.edu>
2013-07-03 21:11:21 +0000

    Adjust script so it does not copy itself, and cause confusion.
    git-svn-id: https://kestrel.ece.cmu.edu/svn/bap/trunk@7770 f55ac123-dd1f-49ad-9780-241693a2721e

--- ocaml-proj.example/deployproj.sh
+++ ocaml-proj.example/deployproj.sh
@@ -6,6 +6,7 @@ set -e
 
 BAPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ".." && pwd )"
 TARGETDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd "../.." && pwd )"
+SCRIPT="$( basename "${BASH_SOURCE[0]}" )"
 
 function copy {
     cp -r $BAPDIR/$1.example $TARGETDIR/$1
@@ -14,5 +15,9 @@ function copy {
 copy ocaml-proj
 copy utils-proj
 copy tests-proj
+
+# Remove script in ocaml-proj to avoid confusion!
+
+rm $TARGETDIR/ocaml-proj/$SCRIPT
 
 echo "Project directories deployed to $TARGETDIR"
