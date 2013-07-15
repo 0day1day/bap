7de16904bcc51b0c4bbb153b8c47d3362b58ff57
Ed Schwartz <moo.git.z.edmcman@xoxy.net>
2013-07-15 18:39:24 -0400

    Add new recovery test.

--- tests/asmir_suite.ml
+++ tests/asmir_suite.ml
@@ -1,9 +1,20 @@
 open OUnit
 
-let open_program_test _ = 
+let open_program_test () =
   ignore (Asmir.open_program "asm/nop");;
+
+let resolve_program_test p =
+  let asmp = Asmir.open_program p in
+  let funcs = Func_boundary.get_function_ranges asmp in
+  let f (n,s,e) = ignore(Asmir_disasm.vsa_at asmp s) in
+  List.iter f funcs
+
+let run_test s () =
+  resolve_program_test s
 
 let suite = "Asmir" >:::
   [
-	"open_program_test" >:: open_program_test;
+    "open_program_test" >:: open_program_test;
+    "resolve_test" >:: run_test "C/recover-hard";
+    "resolve_test_opt" >:: run_test "C/recover-hard-opt";
   ]
