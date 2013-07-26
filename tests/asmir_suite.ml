7a109b6ddba9db39bf640dd17bb6644a35ba04e9
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2013-07-26 15:04:49 -0400

    Add switch condition recovery to unit tests.

--- tests/asmir_suite.ml
+++ tests/asmir_suite.ml
@@ -3,19 +3,37 @@ open OUnit
 let open_program_test () =
   ignore (Asmir.open_program "asm/nop");;
 
-let resolve_program_test p =
+let recover asmp (n,s,e) = ignore(Asmir_disasm.vsa_at asmp s)
+
+let recover_switch asmp (n,s,e) =
+  let _, vsaresult = Asmir_disasm.vsa_at_full asmp s in
+  match vsaresult with
+  | None -> ()
+  | Some vsaresult ->
+    let ssacfg = Switch_condition.add_switch_conditions vsaresult in
+    assert_bool "CFG with switch conditions was recovered" (BatOption.is_some ssacfg)
+
+let resolve_program_test f p =
   let asmp = Asmir.open_program p in
   let funcs = Func_boundary.get_function_ranges asmp in
-  let f (n,s,e) = ignore(Asmir_disasm.vsa_at asmp s) in
-  List.iter f funcs
+  List.iter (f asmp) funcs
 
 let run_resolve_test s () =
-  resolve_program_test s
+  resolve_program_test recover s
+
+let run_resolve_switch_test s () =
+  resolve_program_test recover_switch s
+
+let make_recover_test name fname =
+  [
+    name^"_regular" >:: run_resolve_test fname;
+    name^"_switch" >:: run_resolve_switch_test fname;
+  ]
 
 let suite = "Asmir" >:::
   [
     "open_program_test" >:: open_program_test;
-    "resolve_test" >:: run_resolve_test "C/recover-hard";
-    "resolve_test_opt" >:: run_resolve_test "C/recover-hard-opt";
-    "resolve_test_pointer" >:: run_resolve_test "C/recover-hard-pointer";
   ]
+  @ make_recover_test "resolve_test" "C/recover-hard"
+  @ make_recover_test "resolve_test_opt" "C/recover-hard-opt"
+  @ make_recover_test "resolve_test_pointer" "C/recover-hard-pointer"
