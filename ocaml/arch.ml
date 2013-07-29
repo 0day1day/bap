ed4ebd709fcdb72424852a60a9d1faab14db052f
cdw1 <cdw1@andrew.cmu.edu>
2013-07-29 12:13:45 -0400

    Add Arch.type_of_arch function

--- ocaml/arch.ml
+++ ocaml/arch.ml
@@ -5,3 +5,7 @@ type arch =
 let arch_to_string = function
   | X86_32 -> "X86_32"
   | X86_64 -> "X86_64"
+
+let type_of_arch = function
+  | X86_32 -> Type.Reg 32
+  | X86_64 -> Type.Reg 64
--- ocaml/arch.mli
+++ ocaml/arch.mli
@@ -5,3 +5,5 @@ type arch =
   | X86_64
 
 val arch_to_string : arch -> string
+
+val type_of_arch : arch -> Type.typ
