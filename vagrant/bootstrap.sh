b4d9ccf147ecbf5df4c5876e688379d49c017339
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2013-07-29 21:24:34 -0400

    Use ocaml-nox to save space.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y ocaml ocaml-native-compilers ocaml-findlib camlidl binutils-dev automake libcamomile-ocaml-dev otags libpcre3-dev camlp4-extra bison flex zlib1g-dev libgmp3-dev g++ libtool make
+apt-get install -y ocaml-nox ocaml-native-compilers ocaml-findlib camlidl binutils-dev automake libcamomile-ocaml-dev otags libpcre3-dev camlp4-extra bison flex zlib1g-dev libgmp3-dev g++ libtool make
 
 echo 0 > /proc/sys/kernel/yama/ptrace_scope
 
